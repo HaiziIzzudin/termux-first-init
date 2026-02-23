5f4ae96c576cacf63d69ce28cf93af7b39147eab
haizi <thehub@iziizz.slmail.me>
2026-02-24 05:14:17 +0800

    11

--- termux-init-script.sh
+++ termux-init-script.sh
@@ -1,9 +1,9 @@
 echo "Input password for SSH Login later on:"
 termux-setup-storage
 yes "" | pkg upgrade -y
-yes "" | pkg install openssh iproute2 ffmpeg htop -y
+yes "" | pkg install openssh ffmpeg htop -y
 user=$(whoami)
-ip_addr=$(ip -4 addr show dev wlan0 2>/dev/null | awk '/inet/ {print $2}' | cut -d/ -f1)
+ip_addr=$(ifconfig wlan0 | grep 'inet ' | awk '{print $2}')
 sshd
 echo "ssh service restarted"
 echo "1. Change password first using command 'passwd'"
