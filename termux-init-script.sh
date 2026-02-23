502f77796ddbb44aaa2bd2a951b87c20f2491475
haizi <thehub@iziizz.slmail.me>
2026-02-24 05:27:08 +0800

    refactor: improve SSH login instructions in init script

--- termux-init-script.sh
+++ termux-init-script.sh
@@ -1,4 +1,3 @@
-echo "Input password for SSH Login later on:"
 termux-setup-storage
 yes "" | pkg upgrade -y
 yes "" | pkg install openssh ffmpeg htop -y
@@ -7,5 +6,5 @@ ip_addr=$(ifconfig | grep 'inet 192' | awk '{print $2}')
 sshd
 echo "ssh service restarted"
 echo "1. Change password first using command 'passwd'"
-printf "2. Then login via SSH by\n  $user@$ip_addr -p8022"
+printf "2. Then login via SSH by the cmd below:\n   ssh $user@$ip_addr -p8022"
 echo ""
