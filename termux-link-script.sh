db0a29b49b07187164fcde2ed3f37b9454938255
haizi <thehub@iziizz.slmail.me>
2026-02-24 07:57:09 +0800

    14

--- termux-link-script.sh
+++ termux-link-script.sh
@@ -2,6 +2,4 @@ device_model=$(termux-info | awk '/^Device model:/{getline; gsub(/^[ \t]+|[ \t]+
 user=$(whoami)
 ip_addr=$(ifconfig | grep 'inet 192' | awk '{print $2}')
 read -p "Enter Host Machine IP address: " host_ip_address
-curl -X POST http://${host_ip_address}:8000/register \
--H "Content-Type: application/json" \
--d '{"device_model":"'${device_model}'","user":"'${user}'","ip_address":"'${ip_addr}'"}'
+curl -X POST http://${host_ip_address}:8000/register -H "Content-Type: application/json" -d '{"device_model":"'${device_model}'","user":"'${user}'","ip_address":"'${ip_addr}'"}'
