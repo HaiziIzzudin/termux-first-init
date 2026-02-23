9f0721a268042358ae031c30ddbc124b4c42b3ed
haizi <thehub@iziizz.slmail.me>
2026-02-24 04:36:03 +0800

    Add initial termux-init script for SSH setup and user login

---
+++ termux-init-script.sh
@@ -0,0 +1,8 @@
+pkg upgrade -y && \
+pkg install openssh iproute2 -y && \
+user=whoami && \
+ip_addr=$(ip -4 addr show dev wlan0 2>/dev/null | awk '/inet/ {print $2}' | cut -d/ -f1) && \
+passwd && \
+sshd && \
+echo ssh service restarted && \
+echo "login via ssh by\n $user@$ip_addr -p8022"
