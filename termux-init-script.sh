a2ca24b2742abf1d3dda3007996b285af65dde98
haizi <thehub@iziizz.slmail.me>
2026-02-24 22:36:54 +0800

    enhance termux-init-script.sh to automatically generate SSH key for improved user experience

--- termux-init-script.sh
+++ termux-init-script.sh
@@ -10,6 +10,7 @@ printf "2. Then login via SSH by the cmd below:\n   ssh $user@$ip_addr -p8022"
 echo ""
 read -p "Script paused. \nNext part of this script will attempt to install pubkey on remote (Termux) machine.\nReady? Press enter to continue..." </dev/tty
 
+yes "" | ssh-keygen
 read -p "Please generate ssh-keygen from your host machine (where to connect to). Command:\n\nssh-keygen -t rsa -b 2048 -f id_rsa\n\nHost machine id_rsa: " id_rsa_host </dev/tty
 
 ssh-copy-id -p 8022 -i $id_rsa_host $ip_addr
