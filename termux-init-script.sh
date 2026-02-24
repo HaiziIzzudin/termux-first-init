aedc5212a97517588eade6c90339f5382587b452
haizi <thehub@iziizz.slmail.me>
2026-02-24 22:47:16 +0800

    refactor termux-init-script.sh to improve readability by adding newlines for better separation of script sections

--- termux-init-script.sh
+++ termux-init-script.sh
@@ -11,8 +11,13 @@ echo ""
 read -p "Script paused. \nNext part of this script will attempt to install pubkey on remote (Termux) machine.\nReady? Press enter to continue..." </dev/tty
 
 yes "" | ssh-keygen -t rsa -b 2048 -f id_rsa
+
+echo ""
+echo ""
 read -p "Please generate ssh-keygen from your host machine (where to connect to). Command:\n\nssh-keygen -t rsa -b 2048 -f id_rsa\n\nHost machine id_rsa: " id_rsa_host </dev/tty
 
 ssh-copy-id -p 8022 -i $id_rsa_host $ip_addr
 
+echo ""
+echo ""
 printf "\n\nSuccess! Try login via SSH by the cmd below:\n   ssh $user@$ip_addr -p8022\nIt should not prompt for password, means success."
