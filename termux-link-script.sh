bc27e963bc6aa4452ab9c9eac45c5998b0fa8efc
haizi <thehub@iziizz.slmail.me>
2026-02-24 08:00:41 +0800

    15

--- termux-link-script.sh
+++ termux-link-script.sh
@@ -1,5 +1,6 @@
 device_model=$(termux-info | awk '/^Device model:/{getline; gsub(/^[ \t]+|[ \t]+$/,""); print; exit}')
 user=$(whoami)
 ip_addr=$(ifconfig | grep 'inet 192' | awk '{print $2}')
+echo "Fetch device model done"
 read -p "Enter Host Machine IP address: " host_ip_address
 curl -X POST http://$host_ip_address:8000/register -H "Content-Type: application/json" -d '{"device_model":"'$device_model'","user":"'$user'","ip_address":"'$ip_addr'"}'
